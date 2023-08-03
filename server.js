eebb8b7d5732859cfa4e23fa5695efe73d3b87cd
Naumann <matthias.naumann@koenig-bauer.com>
2023-08-03 13:52:08 -0500

    adding json

--- server.js
+++ server.js
@@ -3,99 +3,116 @@ hosted at https://render.com; connected with GitHub
 https://www.youtube.com/watch?v=2FeymQoKvrk&ab_channel=JavaScriptMastery
 */
 
-import express from "express";
-import fetch from "node-fetch";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import { Transform } from "stream";
-import fs from "fs";
+const express = require('express');
+// const fetch = require('node-fetch');
+const fetch = async () => {
+  try {
+    // Use dynamic import to import the 'node-fetch' module
+    fetch = await import('node-fetch');
+  } catch (err) {
+    console.error('Error importing node-fetch:', err);
+  }
+};
 
-// import fs from 'fs';
-// import {fsPromise} from 'fs/promises';
-// import path from 'path';
+const dotenv = require('dotenv');
+const cors = require('cors');
+const fs = require('fs');
+const { Transform } = require('stream');
+
+const app = express();
+const data = {
+  conversation: require('./model/conversation.json'),
+  setConversation: function (data) {
+    this.conversation = data;
+  },
+}; // data object to be passed to the routes
 
 dotenv.config();
 
-const app = express();
-import conversation from "./model/conversation.js";
-let model = "";
+let model = '';
 
 const saveConversation = async (conversation) => {
   // console.log(`The conversation is: ${JSON.stringify(conversation)}`);
-  fs.writeFileSync(
-    "./model/conversation.js",
-    `const conversation = ${conversation};\nexport default conversation;`
-  );
+  fs.writeFileSync('./model/conversation.json', conversation);
 };
 
 app.use(cors());
 app.use(express.json());
 // app.use(bodyParser.json());
+
+// built-in middleware for json
+app.use(express.json());
+
 app.use(express.urlencoded({ extended: true })); // Add express.urlencoded() middleware
 
-app.post("/reset", async (req, res) => {
-  // console.log(conversation);
+app.post('/reset', async (req, res) => {
   try {
-    const data = req.body;
-    // console.log(data.reset);
-    if (data.reset) {
-      conversation.splice(0, conversation.length);
-      conversation.push({
-        role: "system",
-        content:
-          "The following is a conversation with an AI assistant named Winston. The assistant is helpful, creative, clever, and very friendly. The assistant uses markdown output whenever possible.\n",
-      });
-      saveConversation(JSON.stringify(conversation));
-      // console.log(conversation);
+    console.log(data.conversation);
+    const reqBody = req.body;
+    // console.log(reqBody.reset);
+    if (reqBody.reset) {
+      data.setConversation([
+        {
+          role: 'system',
+          content:
+            'The following is a conversation with an AI assistant named Winston. The assistant is helpful, creative, clever, and very friendly. The assistant uses markdown output whenever possible.\n',
+        },
+      ]);
+      console.log(data.conversation);
       // model = data.model;
-      res.status(200).json({ message: "conversation was reset" });
+      res.status(200).json({ message: 'conversation was reset' });
     }
   } catch (error) {
     console.log(error);
   }
 });
 
-app.get("/chat-history", async (req, res) => {
-  try {
-    res.status(200).json({ message: "success", conversation: conversation });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.post("/chat-data", async (req, res) => {
+app.post('/data', async (req, res) => {
   // console.log(conversation);
   try {
-    const data = req.body;
-    // console.log(data.userMessage);
-    if (conversation[conversation.length - 1].role !== "user") {
-      conversation.push({ role: "user", content: `${data.userMessage}\n` });
+    const reqBody = req.body;
+    // console.log(reqBody);
+    if (data.conversation[data.conversation.length - 1].role !== 'user') {
+      data.setConversation(...data.conversation, {
+        role: 'user',
+        content: reqBody.userMessage,
+      });
     }
-    // console.log(conversation);
-    model = data.model;
-    res.status(200).json({ message: "success", conversation: conversation });
+    console.log(data.conversation);
+    model = reqBody.model;
+    res
+      .status(200)
+      .json({ message: 'success', conversation: data.conversation });
+  } catch (error) {
+    console.log(`here is the error: ${error}`);
+  }
+});
+
+app.get('/history', async (req, res) => {
+  try {
+    res.status(200).json({ message: 'success', conversation: conversation });
   } catch (error) {
     console.log(error);
   }
 });
 
-app.get("/chat-answer", async (req, res) => {
+app.get('/answer', async (req, res) => {
   try {
     res.writeHead(200, {
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache",
-      Connection: "keep-alive",
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      Connection: 'keep-alive',
     });
 
     // make the request to the OpenAI API
     const response = await fetch(process.env.OPENAI_API_URL, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
-        model: model || "gpt-4",
+        model: model || 'gpt-4',
         messages: conversation,
         max_tokens: 1000,
         stream: true, //for the streaming purpose
@@ -103,7 +120,7 @@ app.get("/chat-answer", async (req, res) => {
     });
 
     // let assistantContent = "";
-    conversation.push({ role: "assistant", content: "" });
+    conversation.push({ role: 'assistant', content: '' });
 
     try {
       if (!response.ok) {
@@ -124,12 +141,12 @@ app.get("/chat-answer", async (req, res) => {
         .pipe(
           new Transform({
             transform(chunk, encoding, callback) {
-              const lines = chunk.toString().split("\n");
+              const lines = chunk.toString().split('\n');
               // console.log(lines);
               const parsedLines = lines
-                .map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
-                .filter((line) => line !== "") // Remove empty lines and "[DONE]"
-                .filter((line) => line !== "[DONE]") // Remove empty lines and "[DONE]"
+                .map((line) => line.replace(/^data: /, '').trim()) // Remove the "data: " prefix
+                .filter((line) => line !== '') // Remove empty lines and "[DONE]"
+                .filter((line) => line !== '[DONE]') // Remove empty lines and "[DONE]"
                 .map((line) => JSON.parse(line)); // Parse the JSON string
               // console.log(parsedLines);
               parsedLines.forEach((parsedLine) => {
@@ -147,16 +164,16 @@ app.get("/chat-answer", async (req, res) => {
             },
           })
         )
-        .on("data", () => {
+        .on('data', () => {
           // Consume the data to trigger the 'end' event
         })
-        .on("end", () => {
+        .on('end', () => {
           saveConversation(JSON.stringify(conversation));
           res.end();
           resolve();
         })
-        .on("error", () => {
-          console.log("Error:", err);
+        .on('error', () => {
+          console.log('Error:', err);
           reject(err);
         });
     });
