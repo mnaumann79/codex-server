74db38d3281e3de56578896c72373e45740d8c09
Matthias Naumann <manumann79@gmail.com>
2023-08-30 16:02:18 -0500

    clean up

---
+++ README.md
@@ -0,0 +1,6 @@
+### Deploy Node.js app to Vercel
+
+https://blog.logrocket.com/free-services-deploy-node-js-app/
+
+### Use frontend with backend in one project
+https://www.sammeechward.com/react-proxy-easiest-fix-to-cors-errors?playlist=all-you-need-to-know-about-react-js
--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,4 +1,3 @@
-import fs from "fs";
 import fetch from "node-fetch";
 import { Transform } from "stream";
 import OpenAI from "openai";
@@ -14,39 +13,22 @@ const openai = new OpenAI({
 const data = {
 	conversation: conversation,
 	setConversation: function (data) {
-		// console.log(data);
 		this.conversation = data;
-		// fs.writeFileSync(
-		// 	"./model/conversation.js",
-		// 	`const conversation = ${JSON.stringify(data)};\nexport default conversation;`
-		// );
 	},
 	model: "gpt-4",
 	setModel: function (data) {
-		// console.log(data);
 		this.model = data;
 	}
 }; // data object to be passed to the routes
 
 const resetChat = (req, res) => {
 	const reqBody = req.body;
-	// console.log(reqBody.conversation);
-	data.setConversation(
-		reqBody.conversation
-		// [
-		// 	{
-		// 		role: "system",
-		// 		content:
-		// 			"The following is a conversation with an AI assistant named Winston. The assistant is helpful, creative, clever, and very friendly. The assistant uses markdown output whenever possible. Keep your answers within 100 words.\n"
-		// 	}
-		// ]
-	);
+	data.setConversation(reqBody.conversation);
 	res.status(200).json({ message: "conversation was reset", conversation: data.conversation });
 };
 
 const setUserMessage = (req, res) => {
 	const reqBody = req.body;
-	// console.log(reqBody);
 	if (data.conversation[data.conversation.length - 1].role !== "user") {
 		data.setConversation([
 			...data.conversation,
@@ -60,37 +42,36 @@ const setUserMessage = (req, res) => {
 	data.setModel(reqBody.model);
 
 	res.status(200).json({
-		message: "success",
-		conversation: data.conversation
+		message: "success"
+		// conversation: data.conversation
 	});
 };
 
 const getHistory = (req, res) => {
 	res.status(200).json({
-		message: "success",
+		// message: "success",
 		conversation: data.conversation
 	});
 };
 
 const getAnswerAI = async (req, res) => {
-	
 	res.writeHead(200, {
 		"Content-Type": "text/event-stream",
 		"Cache-Control": "no-cache",
 		Connection: "keep-alive"
 	});
-	
+
 	let assistantContent = "";
-	
+
 	try {
-		console.log(data.conversation);
+		// console.log(data.conversation);
 		const response = await openai.chat.completions.create({
 			model: data.model,
 			messages: data.conversation,
 			max_tokens: 250,
 			stream: true
 		});
-		
+
 		for await (const chunk of response) {
 			const { choices } = chunk;
 			const { delta } = choices[0];
@@ -102,7 +83,7 @@ const getAnswerAI = async (req, res) => {
 			}
 		}
 	} catch (error) {
-		console.log(error)
+		console.log(error);
 		res.write(
 			`data: ${JSON.stringify({
 				message: `${error.message}`
@@ -159,12 +140,12 @@ const getAnswer = async (req, res) => {
 						const lines = chunk.toString().split("\n");
 						// console.log(lines);
 						const parsedLines = lines
-							.map(line => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
-							.filter(line => line !== "") // Remove empty lines and "[DONE]"
-							.filter(line => line !== "[DONE]") // Remove empty lines and "[DONE]"
-							.map(line => JSON.parse(line)); // Parse the JSON string
+							.map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
+							.filter((line) => line !== "") // Remove empty lines and "[DONE]"
+							.filter((line) => line !== "[DONE]") // Remove empty lines and "[DONE]"
+							.map((line) => JSON.parse(line)); // Parse the JSON string
 						// console.log(parsedLines);
-						parsedLines.forEach(parsedLine => {
+						parsedLines.forEach((parsedLine) => {
 							const { choices } = parsedLine;
 							const { delta } = choices[0];
 							const { content } = delta;
